4f46de592e623c5f46e4910a4c5cf5fe664481ab
kdot <alvarezderrick0@gmail.com>
2022-02-09 18:34:04 -0800

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,7 @@ require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+ 
 });
 
 module.exports = mongoose.connection;
